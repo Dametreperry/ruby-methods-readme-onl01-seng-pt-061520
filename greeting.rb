c1099387f27b8b29fe2cdbdbc6bd35158aa22ae8
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-06-19 01:40:03 +0000

    Automatically backed up by Learn

--- greeting.rb
+++ greeting.rb
@@ -1,5 +1,13 @@
-def greeting= "GREETINGS EARTHLINGS"
-end
-
 def greetings_ten_times
-  
+  greetings = "Greetings Earthlings"
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+  puts greetings
+end
